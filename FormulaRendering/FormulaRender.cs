881d3b0c86f746438c58b677977e23e23090de0d
ES <eugene.github@gmail.com>
2013-05-01 01:46:28 +0300

    Refactoring and tests.

--- FormulaRendering/FormulaRender.cs
+++ FormulaRendering/FormulaRender.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;  
 using WallpaperGenerator.Formulas;
 
@@ -10,8 +9,9 @@ namespace WallpaperGenerator.FormulaRendering
         public static RenderedFormulaImage Render(FormulaTreeNode formulaTreeRoot, int width, int height)
         {
             double[] formulaEvaluatedField = GetFormulaEvaluatedField(formulaTreeRoot, width, height);
-            IEnumerable<byte> colors = MapToColors(formulaEvaluatedField);
-            IEnumerable<Rgb> data = colors.Select(c => new Rgb(c, c, c)).ToArray();
+            IEnumerable<double> formulaEvaluatedFieldCountable = formulaEvaluatedField
+                .Select(v => double.IsNaN(v) ? 0 : double.IsNegativeInfinity(v) ? double.MinValue : double.IsPositiveInfinity(v) ? double.MaxValue : v);
+            IEnumerable<Rgb> data = MapToRgb(formulaEvaluatedFieldCountable);
             return new RenderedFormulaImage(data.ToArray(), width, height);
         }
 
@@ -32,13 +32,17 @@ namespace WallpaperGenerator.FormulaRendering
             return field;
         }
         
-        private static IEnumerable<byte> MapToColors(IEnumerable<double> values)
+        private static IEnumerable<Rgb> MapToRgb(IEnumerable<double> values)
         {
-            double expectedValue = GetExpectedValue(values);
-            double threeSigmas = GetThreeSigmas(values);
-            
-            double rangeStart = expectedValue - threeSigmas;
-            double rangeEnd = expectedValue + threeSigmas;
+            double mathExpectation = MathUtilities.MathExpectation(values);
+            double threeSigmas = MathUtilities.ThreeSigmas(values);
+            if (double.IsNegativeInfinity(threeSigmas))
+                threeSigmas = double.MinValue;
+            if (double.IsPositiveInfinity(threeSigmas))
+                threeSigmas = double.MaxValue;  
+
+            double rangeStart = mathExpectation - threeSigmas;
+            double rangeEnd = mathExpectation + threeSigmas;
             if (rangeStart > rangeEnd)
             {
                 double tmp = rangeStart;
@@ -46,44 +50,8 @@ namespace WallpaperGenerator.FormulaRendering
                 rangeEnd = tmp;
             }
 
-            return values.Select(v => (byte)Map(v, rangeStart, rangeEnd, 0, 255));
-        }
-
-        private static double Map(double value, double rangeStart, double rangeEnd, double mappedRangeStart, double mappedRangeEnd)
-        {
-            if (value < rangeStart)
-                value = rangeStart;
-            if (value > rangeEnd)
-                value = rangeEnd;
-
-            double range = rangeEnd - rangeStart;
-            double mappedRange = mappedRangeEnd - mappedRangeStart;
-            double scale = mappedRange/range;
-            return (value - rangeStart) * scale + mappedRangeStart;
-        }
-
-        private static double GetExpectedValue(IEnumerable<double> values)
-        {
-            return values.Sum()/values.Count();
-        }
-
-        private static double GetVariance(IEnumerable<double> values)
-        {
-            double expectedValueOfSquares = GetExpectedValue(values.Select(v => v*v));
-            double expectedValue = GetExpectedValue(values);
-            return expectedValueOfSquares - expectedValue * expectedValue;
-        }
-
-        private static double GetStandardDeviation(IEnumerable<double> values)
-        {
-            double varianse = GetVariance(values);
-            return Math.Sqrt(varianse);
-        }
-
-        private static double GetThreeSigmas(IEnumerable<double> values)
-        {
-            double standartDeviation = GetStandardDeviation(values);
-            return 3*standartDeviation;
+            IEnumerable<byte> colors = values.Select(v => (byte)MathUtilities.Map(v, rangeStart, rangeEnd, 0, 255));
+            return colors.Select(c => new Rgb(c, c, c));
         }
     }
 }
---
+++ FormulaRendering/MathUtilities.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WallpaperGenerator.FormulaRendering
+{
+    public static class MathUtilities
+    {
+        public static double MathExpectation(IEnumerable<double> values)
+        {
+            int count = values.Count();
+            return values.Sum(v => v / count);
+        }
+
+        public static double Variance(IEnumerable<double> values)
+        {
+            int count = values.Count();
+            if (count == 1)
+                return 0;
+
+            double sum = values.Sum();
+            double sumOfSquares = values.Sum(v => v * v);
+            
+            return (sumOfSquares - sum*sum/count)/(count - 1);
+        }
+
+        public static double StandardDeviation(IEnumerable<double> values)
+        {
+            double varianse = Variance(values);
+            return Math.Sqrt(varianse);
+        }
+
+        public static double ThreeSigmas(IEnumerable<double> values)
+        {
+            double standartDeviation = StandardDeviation(values);
+            return 3*standartDeviation;
+        }
+
+        public static double Map(double value, double rangeStart, double rangeEnd, double mappedRangeStart, double mappedRangeEnd)
+        {            
+            if (value < rangeStart)
+                value = rangeStart;
+            if (value > rangeEnd)
+                value = rangeEnd;
+
+            double range = rangeEnd - rangeStart;
+            double mappedRange = mappedRangeEnd - mappedRangeStart;
+            double scale = mappedRange / range;
+            return (value - rangeStart) * scale + mappedRangeStart;
+        }
+    }
+}
