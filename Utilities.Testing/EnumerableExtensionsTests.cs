f1984462f9bcd7e4e1b1a095e13933496b6eefa2
ES <eugene.github@gmail.com>
2013-06-30 16:21:45 +0300

    Random sequence generation with elements probabilities.

---
+++ Utilities.Testing/EnumerableExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MbUnit.Framework;
+
+namespace WallpaperGenerator.Utilities.Testing
+{
+    [TestFixture]
+    public class EnumerableExtensionsTests
+    {
+        [RowTest]
+        [Row(new int[]{}, new int[]{})]
+        [Row(new [] {1, 2, 3, 4}, new [] {1, 3, 6, 10 })]
+        public void TestSelectWithFolding(int[] source, int[] expectedSequence)
+        {
+            IEnumerable<int> foldedSequence = source.SelectWithFolding((p, c) => p + c, 0);
+            CollectionAssert.AreEqual(expectedSequence, foldedSequence.ToArray());
+        }
+
+        [RowTest]
+        [Row(new[] { 1, 2, 3 }, new[]{0.1, 0.3, 0.6}, new [] {2, 2, 2, 3, 3, 3, 2, 3, 3, 2, 3, 2, 1, 1, 3})]
+        [Row(new[] { 1, 2, 3 }, new[] { 0.1, 0.3, 0.7 }, new []{1}, ExpectedException=typeof(ArgumentException))]
+        [Row(new[] { 1, 2, 3 }, new[] { 0.1, 0.3, 0.3, 0.3}, new []{1}, ExpectedException = typeof(ArgumentException))]
+        public void TestTakeRandomWithProbabilites(int[] source, double[] probabilities, int[] expectedSequence)
+        {
+            Random random = new Random(5);
+            IEnumerable<int> sequence = EnumerableExtensions.Repeat(i => source.TakeRandom(random, probabilities), expectedSequence.Length);
+            CollectionAssert.AreEqual(expectedSequence, sequence.ToArray()); 
+        }
+    }
+}
--- Utilities/EnumerableExtensions.cs
+++ Utilities/EnumerableExtensions.cs
@@ -5,11 +5,22 @@ using System.Linq;
 namespace WallpaperGenerator.Utilities
 {
     public static class EnumerableExtensions
-    {
+    {        
         public static IEnumerable<T> Repeat<T>(Func<int, T> func, int count)
         {
             for (int i = 0; i < count; i++)
                 yield return func(i);
+        }
+
+        public static IEnumerable<R> SelectWithFolding<T, R>(this IEnumerable<T> source, Func<R, T, R> func, R initValue)
+        {
+            R previousValue = initValue; 
+            foreach (T e in source)
+            {
+                R value = func(previousValue, e);
+                yield return value;
+                previousValue = value;
+            }
         }
 
         public static IEnumerable<T> Randomize<T>(this IEnumerable<T> source, Random random)
@@ -21,6 +32,31 @@ namespace WallpaperGenerator.Utilities
         {
             int index = random.Next(source.Count()); 
             return source.Skip(index).First();
+        }
+
+        public static T TakeRandom<T>(this IEnumerable<T> source, Random random, IEnumerable<double> elementProbabilities)
+        {
+            if (source.Count() != elementProbabilities.Count())
+            { 
+                throw new ArgumentException("Count of elements isn't equal to count of probabilties.");
+            }
+
+            if (!elementProbabilities.Sum(p => p).Equals(1))
+            {
+                throw new ArgumentException("Sum of probabilties isn't equal to 1.");
+            }
+
+            IEnumerable<double> probabilisticRange = elementProbabilities.SelectWithFolding((p, c) => p + c, 0.0); 
+
+            double r = random.NextDouble();
+            int i = 0;
+            foreach (double p in probabilisticRange)
+            {
+                if (r <= p)
+                    break;
+                i++;
+            }
+            return source.Skip(i).First();
         }
     }
 }
