2e94d1cb027ad597a4d30c7a063634de06882127
eugene sirota <eugene.github@gmail.com>
2013-09-08 01:24:33 +0300

    Some fixes in RandomExtensions.

--- Utilities/RandomExtensions.cs
+++ Utilities/RandomExtensions.cs
@@ -4,38 +4,34 @@ namespace WallpaperGenerator.Utilities
 {
     public static class RandomExtensions
     {
-        public static T GetRandomBetweenTwo<T>(this Random random, T first, T second, double secondOccurenceProbability)
+        public static T GetRandomBetweenTwo<T>(this Random random, T first, T second, double probabilityOfSecond)
         {
             double r = random.NextDouble();
-            return r <= secondOccurenceProbability ? second : first;
+            return r < probabilityOfSecond ? second : first;
         }
 
         public static T GetRandomBetweenThree<T>(this Random random, T first, T second, T third, 
-            double secondOccurenceProbability, double thirdOccurenceProbability)
+            double probabilityOfSecond, double probabilityOfThird)
         {
             double r = random.NextDouble();
-            return r <= secondOccurenceProbability
+            return r < probabilityOfSecond
                 ? second
-                : r <= secondOccurenceProbability + thirdOccurenceProbability
+                : r < probabilityOfSecond + probabilityOfThird
                     ? third
                     : first;                    
         }
 
         public static void RandomlyShrinkBounds(this Random random, ref int lowBound, ref int highBound)
         {
-            double boundShrinkCoeficient = random.NextDouble();
-            int newLowBound = (int)(lowBound * boundShrinkCoeficient);
-            int newHighBound = (int)(highBound * boundShrinkCoeficient);
-            if (newLowBound > newHighBound)
-            {
-                int t = newLowBound;
-                newLowBound = newHighBound;
-                newHighBound = t;
-            }
-            else if (newLowBound == newHighBound)
-            {
+            if (lowBound > highBound)
+                throw new ArgumentException("Low bound can't be bigger then high bound.", "lowBound");
+            
+            double shrinkCoeficient = random.NextDouble();
+            
+            int newLowBound = (int)(lowBound * shrinkCoeficient);
+            int newHighBound = (int)(highBound * shrinkCoeficient);
+            if (newLowBound == newHighBound)
                 return;
-            }
 
             lowBound = newLowBound;
             highBound = newHighBound;
