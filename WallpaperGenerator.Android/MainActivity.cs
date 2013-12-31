b4e9993d44dcc6eecff53aee7a0613f0f118d314
eugene sirota <eugene.github@gmail.com>
2013-12-31 02:57:28 +0200

    Android app project first commit.

---
+++ WallpaperGenerator.Android/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace WallpaperGenerator.Android
+{
+    [Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
