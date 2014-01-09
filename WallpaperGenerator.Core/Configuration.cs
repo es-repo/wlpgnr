4b15af53fa0fcd9880b1d5161116f5d68c166296
eugene sirota <eugene.github@gmail.com>
2014-01-09 02:10:49 +0200

    Some amending.

--- WallpaperGenerator.Core/Configuration.cs
+++ WallpaperGenerator.Core/Configuration.cs
@@ -5,7 +5,7 @@ namespace WallpaperGenerator.Core
     public class Configuration
     {
         public static readonly Bounds<int> DimensionCountBounds = new Bounds<int>(4, 20);
-        public static readonly Bounds<int> MinimalDepthBounds = new Bounds<int>(7, 12);
+        public static readonly Bounds<int> MinimalDepthBounds = new Bounds<int>(8, 14);
         public static readonly Bounds ConstantBounds = new Bounds(-10, 10);
         public static readonly Bounds ConstantProbabilityBounds = new Bounds(0, 1);
         public static readonly Bounds LeafProbabilityBounds = new Bounds(0, 0.5);
