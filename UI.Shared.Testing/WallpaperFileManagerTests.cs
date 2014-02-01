2e4e29994569734602c7d18442fdc904afefecab
eugene sirota <eugene.github@gmail.com>
2014-02-01 21:55:26 +0200

    Fixed more tests.

--- UI.Shared.Testing/WallpaperFileManagerTests.cs
+++ UI.Shared.Testing/WallpaperFileManagerTests.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using MbUnit.Framework;
 using WallpaperGenerator.UI.Core;
+using WallpaperGenerator.UI.Core.Testing;
+using WallpaperGenerator.Utilities;
 
 namespace WallpaperGenerator.UI.Shared.Testing
 {
@@ -13,7 +15,7 @@ namespace WallpaperGenerator.UI.Shared.Testing
         {
             FormulaRenderArguments formulaRenderArguments = FormulaRenderArguments.FromString(
                     "2.1,3.6;1.91,6.87;-0.62,2.26;-4.66,2.25\r\n0,0,0,0;-0.59,-1.43,0.47,0.2;0.21,-0.98,0.88,0.28\r\nSum Atan Pow x2 Cbrt Sin x1 Atan Pow Sin Sum x2 x0 x3");
-            WorkflowRenderResult formulaRenderResult = new WorkflowRenderResult(formulaRenderArguments, null, new FormulaBitmapMock(new Size()) TimeSpan.Zero);
+            WorkflowRenderResult formulaRenderResult = new WorkflowRenderResult(formulaRenderArguments, null, new FormulaBitmapMock(new Size()), TimeSpan.Zero);
 
             WallpaperFileManager wallpaperFileManager = new WallpaperFileManager(Path.GetTempPath());
             Tuple<string, string> filesPath = wallpaperFileManager.Save(formulaRenderResult, false);
